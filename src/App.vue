e9315f292334dafe2a38c5cbcddd7f52f9760f3e
Grace Ifeoluwa Ojedapo <gojedapo@gmail.com>
2023-08-23 17:25:57 +0100

    Finsished

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,8 @@ import HeaderHero from './components/Hero/HeaderHero.vue'
 import FeatureHero from './components/Hero/FeatureHero.vue'
 import ServicesSection from './components/section/ServicesSection.vue'
 import ProjectSection from './components/section/ProjectSection.vue'
+import ContactSection from './components/section/ContactSection.vue'
+import Footer from './components/footer/Footer.vue'
 </script>
 
 <template>
@@ -20,6 +22,10 @@ import ProjectSection from './components/section/ProjectSection.vue'
       <ServicesSection />
 
       <ProjectSection />
+
+      <ContactSection />
+
+      <Footer />
 
     </section>
     
---
+++ src/components/section/ContactSection.vue
@@ -0,0 +1,32 @@
+<template>
+
+    <section class="py-5 bg-coolGray-100">
+        <div class="container mx-auto px-4">
+          <div class="max-w-lg lg:max-w-none mx-auto">
+            <div class="flex flex-wrap -mx-4">
+              <div class="w-full lg:w-1/2 xl:w-7/12 px-4">
+                <div class="max-w-xl py-20 lg:py-40">
+                  <span class="block mb-4 text-sm font-medium">Lorem Ipsum</span>
+                  <div class="max-w-sm sm:max-w-md xl:max-w-none mb-12">
+                    <h1 class="font-heading text-6xl xs:text-8xl sm:text-9xl xl:text-10xl tracking-tighter mb-8">Keep up with our contant</h1>
+                    <p class="max-w-sm text-lg text-coolGray-600">It is a long established fact that a reader will be distracted by the readable content</p>
+                  </div>
+                  <div class="sm:flex max-w-xl">
+                    <div class="w-full mb-3 sm:mb-0 mr-4">
+                      <input class="w-full h-15 py-4 px-8 text-coolGray-500 bg-white border-0 rounded-full" type="email" placeholder="Enter your email">
+                    </div>
+                    <div class="w-full sm:w-auto sm:flex-shrink-0">
+                      <button class="inline-block w-full sm:w-auto h-15 py-4 px-8 text-center font-medium text-white leading-none bg-black hover:bg-blue-500 rounded-full transition duration-150" type="submit">Sign Up</button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="w-full lg:w-1/2 xl:w-5/12 px-4">
+                <img class="block h-full w-full rounded-3xl object-cover" src="https://shuffle.dev/asitrastudio-assets/newsletter/photo1.png" alt="">
+              </div>
+            </div>
+          </div>
+        </div>
+    </section>
+
+</template>
